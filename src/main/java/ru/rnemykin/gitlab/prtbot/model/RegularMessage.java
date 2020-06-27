357853e1dac7f0acb2a10766fd385b70eed37f31
rnemykin <nemykin.roman@gmail.com>
2020-06-27 21:01:45 +0300

    ~

---
+++ src/main/java/ru/rnemykin/gitlab/prtbot/model/RegularMessage.java
@@ -0,0 +1,12 @@
+package ru.rnemykin.gitlab.prtbot.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Table
+@Entity
+public class RegularMessage extends AbstractMessage {
+}
